888b916638eac125e2b82e23decb9823af3f9cb8
Caleb Lee <caleb@Calebs-MacBook-Pro-3.local>
2020-03-28 16:44:30 -0500

    Build rough header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import portrait from '../img/cl-portrait.jpg';
 
 class Navbar extends Component {
   render() {
     return (
       <header class="header">
-        
+        <div class="container">
+          <a href="#">
+            <img src={portrait} alt="Portrait" className="avatar"/>
+          </a>
+          <div className="avatar__caption">
+            <h1 className="header__title">Caleb Lee</h1>
+            <p className="header__subtitle">Web Developer</p>
+            <p className="header__location">Nashville, TN</p>
+          </div>
+        </div>
       </header>
     )
   }
